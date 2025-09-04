80451dee9242fcc52189d7917783f326c866f80e
wpfarm <info@wpfarm.com>
2025-09-04 11:57:16 -0400

    Delete loader.php

--- loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-require_once (wpmpg_path . 'classes/common.php');
-require_once (wpmpg_path . 'classes/postype.php');
-require_once (wpmpg_path . 'classes/taxoterm.php');
-require_once (wpmpg_path . 'classes/class.php');
-require_once (wpmpg_path . 'classes/elementor-cpf.php');
